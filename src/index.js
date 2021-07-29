f674cdd041f7f7a2bb5e60ab442b8de2b86999b5
Edward Felix <eddalm04@gmail.com>
2021-07-29 03:58:40 -0400

    Configure index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import rootReducer from './store/reducers/rootReducer';
+
+export const middlewares = [thunk];
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
